39d70c4efa2840b4a7f7c18850d3745c10cd2edb
pabloApoca <pablo_mtv08@hotmail.com>
2020-09-12 00:53:53 -0300

    Ya esta funcionando el CRUD ABM de Empleados con interfaz de doble entrada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './modelos/Empleado';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  EmpleadoArray : Empleado[] = [
+    {id: 1, nombre: "Pablo", pais: "Argentina"},
+    {id: 2, nombre: "Mariana", pais: "Costa Rica"},
+    {id: 3, nombre: "Juanito", pais: "Mexico"},
+  ];
+
+  seleccionEmpleado: Empleado = new Empleado();
+
+  editarEmpleado(empleado : Empleado){
+    this.seleccionEmpleado = empleado;
+  }
+
+  agregarOEditar(){
+    if(this.seleccionEmpleado.id===0){
+      this.seleccionEmpleado.id = this.EmpleadoArray.length +1;
+      this.EmpleadoArray.push(this.seleccionEmpleado);
+    }
+
+    this.seleccionEmpleado = new Empleado();
+  }
+
+  eliminar(){
+    if(confirm('¿Estas seguro de querer eliminar a '+ this.seleccionEmpleado.nombre+ ' ?' )){
+      this.EmpleadoArray = this.EmpleadoArray.filter(x => x != this.seleccionEmpleado);
+      this.seleccionEmpleado = new Empleado();
+    }
+    
+  
+  }
+
 }
---
+++ src/app/modelos/empleado.ts
@@ -0,0 +1,5 @@
+export class Empleado {
+    id: number = 0;
+    nombre: string;
+    pais: string;
+}
